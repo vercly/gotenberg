7da3fb43ff595f6c05efc7c392aee4708c7775b3
Andrzej Osipienko <andrzej.osipienko@hobly.pl>
2024-10-23 08:59:52 +0200

    add qpdf for merging

--- pkg/standard/imports.go
+++ pkg/standard/imports.go
@@ -14,6 +14,6 @@ import (
 	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/pdfengines"
 	// _ "github.com/gotenberg/gotenberg/v8/pkg/modules/pdftk"
 	// _ "github.com/gotenberg/gotenberg/v8/pkg/modules/prometheus"
-	// _ "github.com/gotenberg/gotenberg/v8/pkg/modules/qpdf"
+	_ "github.com/gotenberg/gotenberg/v8/pkg/modules/qpdf"
 	// _ "github.com/gotenberg/gotenberg/v8/pkg/modules/webhook"
 )
